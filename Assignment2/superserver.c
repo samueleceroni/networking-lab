8fadff4952202bf30486ba02db33657c0cfa89f3
Samuele Ceroni <samuele.ceroni@gmail.com>
2019-11-25 22:33:03 +0100

    implement assignment 2

--- Assignment2/superserver.c
+++ Assignment2/superserver.c
@@ -4,11 +4,13 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<sys/time.h>
+#include<sys/wait.h>
 #include<netinet/in.h>
 #include<signal.h>
 #include<errno.h>
 #include<stdbool.h>
 #include<ctype.h>
+#include<unistd.h>
 
 // Buffer sizes
 #define PROTOCOL_TYPE_SIZE 4
@@ -20,6 +22,16 @@
 // Exit statuses
 #define EXIT_READ_ERROR 10
 #define EXIT_CONF_ERROR 11
+#define EXIT_SOCKET_CREATION_ERROR 12
+#define EXIT_SOCKET_BIND_ERROR 13
+#define EXIT_LISTEN_ERROR 14
+#define EXIT_ACCEPT_ERROR 15
+#define EXIT_FORK_ERROR 16
+#define EXIT_SELECT_ERROR 17
+#define EXIT_CLOSE_ERROR 18
+#define EXIT_DUP_ERROR 19
+#define EXIT_EXECLE_ERROR 20
+#define EXIT_WAIT_ERROR 21
 
 // Constants
 #define PROTOCOL_UDP "udp"
@@ -27,6 +39,7 @@
 #define MODE_WAIT "wait"
 #define MODE_NOWAIT "nowait"
 #define PORT_MAX 65535
+#define MAX_TCP_PENDING_CONNECTIONS 8
 
 typedef struct {
 	char protocol[PROTOCOL_TYPE_SIZE]; // 'tcp', 'udp'
@@ -47,10 +60,40 @@ typedef struct {
 void die(int error) {
 	switch(error) {
 		case EXIT_READ_ERROR:
-			fprintf(stderr, "An error occurred reading configuration file!");
+			perror("An error occurred reading configuration file");
 			break;
 		case EXIT_CONF_ERROR:
-			fprintf(stderr, "The configuration file is not formatted correctly!");
+			perror("The configuration file is not formatted correctly");
+			break;
+		case EXIT_SOCKET_CREATION_ERROR:
+			perror("The creation of the socket was unsuccesful");
+			break;
+		case EXIT_SOCKET_BIND_ERROR:
+			perror("The bind of the port was unsuccesful");
+			break;
+		case EXIT_LISTEN_ERROR:
+			perror("The listen returned an error");
+			break;
+		case EXIT_ACCEPT_ERROR:
+			perror("Cannot accept connection");
+			break;
+		case EXIT_FORK_ERROR:
+			perror("Cannot create a forked process");
+			break;
+		case EXIT_SELECT_ERROR:
+			perror("The select operation returned an error");
+			break;
+		case EXIT_CLOSE_ERROR:
+			perror("The close operation returned an error");
+			break;
+		case EXIT_DUP_ERROR:
+			perror("The dup operation returned an error");
+			break;
+		case EXIT_EXECLE_ERROR:
+			perror("The execle operation returned an error");
+			break;
+		case EXIT_WAIT_ERROR:
+			perror("The wait operation returned an error");
 			break;
 	}
 	exit(error);
@@ -107,6 +150,7 @@ ServiceDataVector read_configuration(FILE *stream) {
 		} while(is_empty(line));
 
 		ServiceData *current = &config.services[index];
+		current->pid = 0;
 
 		int count = sscanf(line, formatString,
 			current->path, current->protocol, current->port, current->mode);
@@ -122,6 +166,92 @@ ServiceDataVector read_configuration(FILE *stream) {
 	return config;
 }
 
+bool isServiceTcp(ServiceData* config) {
+	return strcmp(config->protocol, PROTOCOL_TCP) == 0;
+}
+
+bool isServiceWait(ServiceData* config) {
+	return strcmp(config->mode, MODE_WAIT) == 0;
+}
+
+void try_create_socket(ServiceData *config, bool isTcp){
+	config->socketFD = socket(AF_INET, isTcp ? SOCK_STREAM : SOCK_DGRAM, isTcp ? IPPROTO_TCP : IPPROTO_UDP);
+	if(config->socketFD < 0)
+		die(EXIT_SOCKET_CREATION_ERROR);
+}
+
+void try_bind(ServiceData *config, struct sockaddr_in serverAddr){
+	if (bind(config->socketFD, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+		die(EXIT_SOCKET_BIND_ERROR);
+}
+
+void try_listen(ServiceData *config){
+	if(listen(config->socketFD, MAX_TCP_PENDING_CONNECTIONS) < 0)
+		die(EXIT_LISTEN_ERROR);
+}
+
+int try_accept(ServiceData *config){
+	int acceptResult = accept(config->socketFD, NULL, NULL);
+	if(acceptResult < 0)
+		die(EXIT_ACCEPT_ERROR);
+	return acceptResult;
+}
+
+void try_close(int socketFD) {
+	if (close(socketFD) < 0) {
+		die(EXIT_CLOSE_ERROR);
+	}
+}
+
+pid_t try_fork() {
+	pid_t result = fork();
+	if (result < 0)
+		die(EXIT_FORK_ERROR);
+	return result;
+}
+
+void try_dup(int socketFD) {
+	if (dup(socketFD) < 0)
+		die(EXIT_DUP_ERROR);
+}
+
+pid_t try_wait(int* status) {
+	pid_t pid = wait(status);
+	if (pid < 0)
+		die(EXIT_WAIT_ERROR);
+	return pid;
+}
+
+struct sockaddr_in get_initialized_server_addr(ServiceData* config){
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(atoi(config->port)); // Convert to network byte order
+	serverAddr.sin_addr.s_addr = INADDR_ANY; // Bind to any address
+	return serverAddr;
+}
+
+void initialize_service(ServiceData *config) {
+	bool isTcp = isServiceTcp(config);
+
+	struct sockaddr_in serverAddr = get_initialized_server_addr(config);
+
+	try_create_socket(config, isTcp);
+	try_bind(config, serverAddr);
+	if (isTcp) try_listen(config);
+}
+
+// Initialize all services, returns the highest file descriptor + 1 (useful for select).
+int initialize_all(ServiceDataVector *config) {
+	int maxFd = -1;
+	for (size_t i = 0; i < config->size; i++) {
+		initialize_service(&config->services[i]);
+		if (config->services[i].socketFD > maxFd) {
+			maxFd = config->services[i].socketFD;
+		}
+	}
+	return maxFd + 1;
+}
+
 // TODO remove, debug only
 void print_config(ServiceDataVector config) {
 	for (int i = 0; i < config.size; i++) {
@@ -135,39 +265,111 @@ void free_services(ServiceDataVector *config) {
 	config->size = 0;
 }
 
+// Never returns
+void handle_service(int inputSocketFD, ServiceData *config, char * const envp[]) {
+	try_close(0);
+	try_dup(inputSocketFD);
+
+	try_close(1);
+	try_dup(inputSocketFD);
+
+	try_close(2);
+	try_dup(inputSocketFD);
+
+	if (execle(config->path, config->name, envp) < 0)
+		die(EXIT_EXECLE_ERROR);
+}
+
 //Function prototype devoted to handle the death of the son process
 void handle_signal (int sig);
+
+ServiceDataVector config;
+fd_set socketsSet;
 
 int  main(int argc,char **argv,char **env){ // NOTE: env is the variable to be passed, as last argument, to execle system-call
 	// Other variables declaration goes here
 	
 	// Configuration loading
 	FILE *fp = fopen("superserver.conf", "r"); // TODO more checks
-	ServiceDataVector config = read_configuration(fp);
+	config = read_configuration(fp);
 	fclose(fp);
 	
-	// Server behavior implementation goes here
-	
 	print_config(config); // TODO remove, debug only
+
+	printf("initialize_all\n");
+	// Server behavior implementation goes here
+	int highestFd = initialize_all(&config);
+	printf("highestFd%d\n", highestFd);
 	
-	signal (SIGCHLD,handle_signal); /* Handle signals sent by son processes - call this function when it's ought to be */
-	
+	FD_ZERO(&socketsSet);
+	for (size_t i = 0; i < config.size; i++) {
+		printf("socket set %d\n", i);
+		FD_SET(config.services[i].socketFD, &socketsSet);
+	}
+
+	// Handle signals sent by son processes
+	signal (SIGCHLD, handle_signal);
+
+	while (true) {
+		fd_set readSet = socketsSet;
+		if (select(highestFd, &readSet, NULL, NULL, NULL) < 0 && errno != EINTR) {
+			die(EXIT_SELECT_ERROR);
+		}
+
+		for (size_t i = 0; i < config.size; i++) {
+			if (FD_ISSET(config.services[i].socketFD, &readSet)) {
+				bool isTcp = isServiceTcp(&config.services[i]);
+				int receiveSocketFD;
+				if (isTcp) {
+					receiveSocketFD = try_accept(&config.services[i]);
+				} else {
+					receiveSocketFD = config.services[i].socketFD;
+				}
+
+				pid_t pid = try_fork();
+
+				if (pid == 0) {
+					if (isTcp) {
+						try_close(config.services[i].socketFD);
+					}
+					handle_service(receiveSocketFD, &config.services[i], env); // Never returns
+				}
+
+				// In the father
+				if (isTcp) {
+					try_close(receiveSocketFD);
+				}
+
+				if (isServiceWait(&config.services[i])) {
+					FD_CLR(config.services[i].socketFD, &socketsSet);
+					config.services[i].pid = pid;
+				}
+			}
+		}
+	}
+
 	free_services(&config);
 	return 0;
 }
 
 // handle_signal implementation
 void handle_signal (int sig){
-	// Call to wait system-call goes here
-	
-	
+	int childStatus;
+	pid_t childPid;
 	switch (sig) {
-		case SIGCHLD : 
-			// Implementation of SIGCHLD handling goes here	
-			
-			
+		case SIGCHLD:
+			childPid = try_wait(&childStatus);
+			if (!WIFEXITED(childStatus)) {
+				fprintf(stderr, "A child with PID %d exited with code %d\n", childPid, WEXITSTATUS(childStatus));
+			}
+			for (size_t i = 0; i < config.size; i++) {
+				if (config.services[i].pid == childPid) {
+					FD_SET(config.services[i].socketFD, &socketsSet);
+					config.services[i].pid = 0;
+					break;
+				}
+			}
 			break;
-		default : printf ("Signal not known!\n");
-			break;
+		default : printf ("Signal not known!\n"); break;
 	}
 }
