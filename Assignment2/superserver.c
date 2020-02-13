63bec5c95c88f8f530c00bcc092ce42a41671276
Carlovan <giulio.carlassare@gmail.com>
2020-02-13 23:12:42 +0100

    Some code refactoring and cleaning

--- Assignment2/superserver.c
+++ Assignment2/superserver.c
@@ -106,86 +106,16 @@ void print_error(int error) {
 	}
 }
 
-// Terminates the program with a custom error code
+// Terminates the program with a custom error code, printing a relevant error message
 void die(int error) {
 	print_error(error);
 	exit(error);
 }
 
-// Checks if the given null-terminated string contains only whitespaces
-bool is_empty(char *s) {
-	for(; *s != '\0'; s++) {
-		if (!isspace(*s)) {
-			return false;
-		}
-	}
-	return true;
-}
-
-// Counts non-empty lines in a stream
-size_t count_lines(FILE *stream) {
-	size_t lines = 0;
-	bool prevNewline = true;
-	char c;
-	while((c = fgetc(stream)) != EOF) {
-		if (c != '\n' && !isspace(c) && prevNewline) {
-			lines++;
-			prevNewline = false;
-		} else if (c == '\n') {
-			prevNewline = true;
-		}
-	}
-	return lines;
-}
-
-
-ServiceDataVector read_configuration(FILE *stream) {
-	// Initialize the vector
-	ServiceDataVector config;
-	long fileBegin = ftell(stream);
-	config.size = count_lines(stream);
-	fseek(stream, fileBegin, SEEK_SET);
-	config.services = (ServiceData*)malloc(config.size * sizeof(ServiceData));
-
-	// Generate the format string to read parameters from a line
-	char formatString[7*4];
-	sprintf(formatString, "%%%ds %%%ds %%%ds %%%ds",
-		MAX_NAME_SIZE-1, PROTOCOL_TYPE_SIZE-1, PORT_NUMBER_SIZE-1, SERVICE_MODE_SIZE-1);
-
-	// Read and parse line by line
-	char line[MAX_LINE_SIZE];
-	for (size_t index = 0; index < config.size; index++) {
-		do {
-			char* result = fgets(line, MAX_LINE_SIZE, stream);
-			if (result == NULL && !feof(stream)) {
-				die(EXIT_READ_ERROR);
-			}
-		} while(is_empty(line));
-
-		ServiceData *current = &config.services[index];
-		current->pid = 0;
-
-		int count = sscanf(line, formatString,
-			current->path, current->protocol, current->port, current->mode);
-		if (count != 4 ||
-			(strcmp(PROTOCOL_UDP, current->protocol) != 0 && strcmp(PROTOCOL_TCP, current->protocol) != 0) ||
-			(strcmp(MODE_WAIT, current->mode) != 0 && strcmp(MODE_NOWAIT, current->mode) != 0) ||
-			(atoi(current->port) <= 0 || atoi(current->port) > PORT_MAX)) {
-			die(EXIT_CONF_ERROR);
-		}
-		const char *lastSlash = strrchr(current->path, '/');
-		strcpy(current->name, lastSlash == NULL ? current->path : lastSlash+1);
-	}
-	return config;
-}
-
-bool isServiceTcp(ServiceData* config) {
-	return strcmp(config->protocol, PROTOCOL_TCP) == 0;
-}
-
-bool isServiceWait(ServiceData* config) {
-	return strcmp(config->mode, MODE_WAIT) == 0;
-}
+// ========================= System calls wrappers =========================
+// The following functions wraps system call and handles any error occurred.
+// Functions starting with `child_` are meant to be used in a child process,
+// the others in the parent process.
 
 void try_create_socket(ServiceData *config, bool isTcp){
 	config->socketFD = socket(AF_INET, isTcp ? SOCK_STREAM : SOCK_DGRAM, isTcp ? IPPROTO_TCP : IPPROTO_UDP);
@@ -216,22 +146,11 @@ void try_close(int socketFD) {
 	}
 }
 
-void child_try_close(int socketFD) {
-	if (close(socketFD) < 0) {
-		exit(CHILD_EXIT_CLOSE_ERROR);
-	}
-}
-
 pid_t try_fork() {
 	pid_t result = fork();
 	if (result < 0)
 		die(EXIT_FORK_ERROR);
 	return result;
-}
-
-void child_try_dup(int socketFD) {
-	if (dup(socketFD) < 0)
-		exit(CHILD_EXIT_DUP_ERROR);
 }
 
 pid_t try_wait(int* status) {
@@ -254,6 +173,105 @@ bool try_select(int highestFd, fd_set* readSet){
 	return true;
 }
 
+void child_try_close(int socketFD) {
+	if (close(socketFD) < 0) {
+		exit(CHILD_EXIT_CLOSE_ERROR);
+	}
+}
+
+void child_try_dup(int socketFD) {
+	if (dup(socketFD) < 0)
+		exit(CHILD_EXIT_DUP_ERROR);
+}
+
+// ============================ Helper functions ===========================
+
+// Checks if the given null-terminated string contains only whitespaces
+bool is_empty(char *s) {
+	for (; *s != '\0'; s++) {
+		if (!isspace(*s)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// Counts non-empty lines in a stream (a line is empty if there are no chars or only whites)
+size_t count_lines(FILE *stream) {
+	size_t lines = 0;
+	bool prevNewline = true;
+	char c;
+	while ((c = fgetc(stream)) != EOF) {
+		if (c != '\n' && !isspace(c) && prevNewline) {
+			lines++;
+			prevNewline = false;
+		} else if (c == '\n') {
+			prevNewline = true;
+		}
+	}
+	return lines;
+}
+
+ServiceDataVector read_configuration(FILE *stream) {
+	// Initialize the conf vector counting the lines in conf file
+	ServiceDataVector config;
+	long fileBegin = ftell(stream);
+	config.size = count_lines(stream);
+	fseek(stream, fileBegin, SEEK_SET);
+	config.services = (ServiceData*)malloc(config.size * sizeof(ServiceData));
+
+	// Generate the format string to read parameters from a line
+	char formatString[7*4];
+	sprintf(formatString, "%%%ds %%%ds %%%ds %%%ds",
+		MAX_NAME_SIZE-1, PROTOCOL_TYPE_SIZE-1, PORT_NUMBER_SIZE-1, SERVICE_MODE_SIZE-1);
+
+	// Read and parse line by line
+	char line[MAX_LINE_SIZE];
+	for (size_t index = 0; index < config.size; index++) {
+		do { // Find the next non-empty line
+			char* result = fgets(line, MAX_LINE_SIZE, stream);
+			if (result == NULL && !feof(stream)) {
+				die(EXIT_READ_ERROR);
+			}
+		} while(is_empty(line)); // TODO check if result is not NULL
+
+		ServiceData *current = &config.services[index];
+		current->pid = 0;
+
+		// Extract data from the line and check validity
+		int count = sscanf(line, formatString,
+			current->path, current->protocol, current->port, current->mode);
+		if (count != 4 ||
+			(strcmp(PROTOCOL_UDP, current->protocol) != 0 && strcmp(PROTOCOL_TCP, current->protocol) != 0) ||
+			(strcmp(MODE_WAIT, current->mode) != 0 && strcmp(MODE_NOWAIT, current->mode) != 0) ||
+			(atoi(current->port) <= 0 || atoi(current->port) > PORT_MAX)) {
+			die(EXIT_CONF_ERROR);
+		}
+		const char *lastSlash = strrchr(current->path, '/'); // Extract executable name from path
+		strcpy(current->name, lastSlash == NULL ? current->path : lastSlash+1);
+	}
+	return config;
+}
+
+// Reads configuration from the right file
+ServiceDataVector read_server_configuration(){
+	FILE *fp = fopen(SUPERSERVER_CONF_FILE_NAME, "r");
+	if(fp == NULL)
+		die(EXIT_SUPERSERVER_CONFIG_FILE_ERROR);
+	ServiceDataVector config = read_configuration(fp);
+	fclose(fp);
+	return config;
+}
+
+bool is_service_tcp(ServiceData* config) {
+	return strcmp(config->protocol, PROTOCOL_TCP) == 0;
+}
+
+bool is_service_wait(ServiceData* config) {
+	return strcmp(config->mode, MODE_WAIT) == 0;
+}
+
+// Gets the server address for binding the socket of the given service
 struct sockaddr_in get_initialized_server_addr(ServiceData* config){
 	struct sockaddr_in serverAddr;
 	serverAddr.sin_family = AF_INET;
@@ -262,8 +280,9 @@ struct sockaddr_in get_initialized_server_addr(ServiceData* config){
 	return serverAddr;
 }
 
+// Initializes the socket for the given service
 void initialize_service(ServiceData *config) {
-	bool isTcp = isServiceTcp(config);
+	bool isTcp = is_service_tcp(config);
 
 	struct sockaddr_in serverAddr = get_initialized_server_addr(config);
 
@@ -272,8 +291,8 @@ void initialize_service(ServiceData *config) {
 	if (isTcp) try_listen(config);
 }
 
-// Initialize all services, returns the highest file descriptor + 1 (useful for select).
-int initialize_all_configs(ServiceDataVector *config) {
+// Initialize all services, returns the highest file descriptor + 1 (useful for select)
+int initialize_all_services(ServiceDataVector *config) {
 	int maxFd = -1;
 	for (size_t i = 0; i < config->size; i++) {
 		initialize_service(&config->services[i]);
@@ -292,12 +311,13 @@ void print_config(ServiceDataVector config) {
 	}
 }
 
+// Frees a ServiceDataVector memory
 void free_services(ServiceDataVector *config) {
 	free(config->services);
 	config->size = 0;
 }
 
-// Never returns
+// Creates a child process for the servicel; never returns
 void spawn_service(int inputSocketFD, ServiceData *config, char * const envp[]) {
 	child_try_close(0);
 	child_try_dup(inputSocketFD);
@@ -316,18 +336,10 @@ void spawn_service(int inputSocketFD, ServiceData *config, char * const envp[]) 
 	}
 }
 
-ServiceDataVector read_server_configuration(){
-	FILE *fp = fopen(SUPERSERVER_CONF_FILE_NAME, "r");
-	if(fp == NULL)
-		die(EXIT_SUPERSERVER_CONFIG_FILE_ERROR);
-	ServiceDataVector config = read_configuration(fp);
-	fclose(fp);
-	return config;
-}
-
+// Handles a connection request for a service
 void handle_service(ServiceData* config, char **env, fd_set* socketsSet){
-	bool isTcp = isServiceTcp(config);
-	int receiveSocketFD;
+	bool isTcp = is_service_tcp(config);
+	int receiveSocketFD; // Socket to be used in the child
 	if (isTcp) {
 		receiveSocketFD = try_accept(config);
 	} else {
@@ -335,46 +347,45 @@ void handle_service(ServiceData* config, char **env, fd_set* socketsSet){
 	}
 
 	pid_t pid = try_fork();
-
-	if (pid == 0) {
+	if (pid == 0) { // In the child
 		if (isTcp) {
 			try_close(config->socketFD);
 		}
 		spawn_service(receiveSocketFD, config, env); // Never returns
 	}
 
-	// In the father
+	// From now on in the father
 	if (isTcp) {
-		try_close(receiveSocketFD);
+		try_close(receiveSocketFD); // Close data TCP socket
 	}
-
-	if (isServiceWait(config)) {
+	if (is_service_wait(config)) {
+		// Remove the socket from the select set
 		FD_CLR(config->socketFD, socketsSet);
+		// and save the child PID
 		config->pid = pid;
 	}
 }
 
-
-//Function prototype devoted to handle the death of the son process
-void handle_signal (int sig);
-
-ServiceDataVector config;
-fd_set socketsSet;
-
 void initialize_socket_set(ServiceDataVector config, fd_set *socketsSet){
 	FD_ZERO(socketsSet);
 	for (size_t i = 0; i < config.size; i++) {
-		printf("socket set %d\n", i);
 		FD_SET(config.services[i].socketFD, socketsSet);
 	}
 }
 
+//Function prototype devoted to handle the death of the son process
+void handle_signal (int sig);
+
+// These needs to be global because they are used in the signal handler
+ServiceDataVector config;
+fd_set socketsSet;
+
 void main_loop(int highestFd, char **env){
 	while(true) {
-		fd_set readSet = socketsSet;
+		fd_set readSet = socketsSet; // Copy the socketSet (select will modify it)
 		bool somethingReady = try_select(highestFd, &readSet);
 
-		if (somethingReady) {
+		if (somethingReady) { // Otherwise it has been interrupted by a signal
 			for (size_t i = 0; i < config.size; i++) {
 				if (FD_ISSET(config.services[i].socketFD, &readSet)) {
 					handle_service(&config.services[i], env, &socketsSet);
@@ -384,23 +395,17 @@ void main_loop(int highestFd, char **env){
 	}
 }
 
-int  main(int argc,char **argv,char **env){ // NOTE: env is the variable to be passed, as last argument, to execle system-call
-	// Other variables declaration goes here
-	
+int main(int argc, char **argv, char **env) {
 	// Configuration loading
 	config = read_server_configuration();
-	
+
 	print_config(config); // TODO remove, debug only
 
-	printf("initialize_all\n");
-	// Server behavior implementation goes here
-	int highestFd = initialize_all_configs(&config);
-	printf("highestFd%d\n", highestFd);
-	
+	int highestFd = initialize_all_services(&config);
 	initialize_socket_set(config, &socketsSet);
 
 	// Handle signals sent by son processes
-	signal (SIGCHLD, handle_signal);
+	signal(SIGCHLD, handle_signal);
 
 	main_loop(highestFd, env);
 
@@ -408,8 +413,8 @@ int  main(int argc,char **argv,char **env){ // NOTE: env is the variable to be p
 	return 0;
 }
 
-// handle_signal implementation
-void handle_signal (int sig){
+// Signal handler function
+void handle_signal(int sig) {
 	int childStatus;
 	pid_t childPid;
 	switch (sig) {
@@ -418,7 +423,7 @@ void handle_signal (int sig){
 			if (WEXITSTATUS(childStatus) != 0) {
 				fprintf(stderr, "A child with PID %d exited with code %d\n", childPid, WEXITSTATUS(childStatus));
 				print_error(WEXITSTATUS(childStatus));
-			}
+			} // TODO maybe an else here?
 			for (size_t i = 0; i < config.size; i++) {
 				if (config.services[i].pid == childPid) {
 					printf("found pid: %s\n", config.services[i].name);
@@ -428,6 +433,8 @@ void handle_signal (int sig){
 				}
 			}
 			break;
-		default : printf ("Signal not known!\n"); break;
+		default:
+			printf("Signal not known!\n");
+			break;
 	}
 }
